dfdec464ee26f960eed2484d3b0b380ce8f2201f
SlimDReaper <ronnyduran35@gmail.com>
2025-03-19 21:39:22 -0400

    Creacion clase Program

--- GestionContactosApp/GestionContactos.Api/Program.cs
+++ GestionContactosApp/GestionContactos.Api/Program.cs
@@ -1,25 +1,44 @@
+using GestionContactos.Application.Services;
+using GestionContactos.Domain.Data;
+using GestionContactos.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// Agregar servicios al contenedor.
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+// Configuración de la base de datos
+builder.Services.AddDbContext<GestionContactosContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Inyección de dependencias
+builder.Services.AddScoped<DatoUsuarioRepository>();
+builder.Services.AddScoped<DatoUsuarioService>();
+
+// Configuración de Swagger
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+}
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configuración de middlewares
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+app.UseCors(policy =>
+    policy.AllowAnyOrigin()
+          .AllowAnyMethod()
+          .AllowAnyHeader());
+
 
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
