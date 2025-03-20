b526bb706739f1eb669baeee836149fb114dab77
SlimDReaper <ronnyduran35@gmail.com>
2025-03-20 01:56:52 -0400

    Creación ApiResponse

---
+++ GestionContactosApp/GestionContactos.Frontend/Models/ApiResponse.cs
@@ -0,0 +1,11 @@
+﻿public class ApiResponse<T>
+{
+    public string Message { get; set; }
+    public T Data { get; set; }
+
+    public ApiResponse(string message, T data)
+    {
+        Message = message;
+        Data = data;
+    }
+}
