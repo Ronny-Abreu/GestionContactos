db47fb4252535e93cc9ee26000a9bde077f5b9e6
SlimDReaper <ronnyduran35@gmail.com>
2025-03-19 21:35:05 -0400

    Creación clase "GestionContactosContextFactory" para la migración a la bdd

--- GestionContactosApp/GestionContactos.Domain/Data/GestionContactosContextFactory.cs
+++ GestionContactosApp/GestionContactos.Domain/Data/GestionContactosContextFactory.cs
@@ -1,12 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
 
 namespace GestionContactos.Domain.Data
 {
-    class GestionContactosContextFactory
+    public class GestionContactosContextFactory : IDesignTimeDbContextFactory<GestionContactosContext>
     {
+        public GestionContactosContext CreateDbContext(string[] args)
+        {
+         
+            var optionsBuilder = new DbContextOptionsBuilder<GestionContactosContext>();
+            
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../GestionContactosApp.Api")) // Ruta al proyecto API
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+
+            return new GestionContactosContext(optionsBuilder.Options);
+        }
     }
 }
