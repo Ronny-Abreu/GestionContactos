f2f1fb268a3d942c0529ea529051b8eb21a0efc5
SlimDReaper <ronnyduran35@gmail.com>
2025-03-22 17:06:51 -0400

    Inicialización nueveamente de la migración

---
+++ GestionContactosApp/GestionContactos.Infrastructure/Migrations/20250322205716_InitialMigration.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace GestionContactos.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Datos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Apellido = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Correo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Telefono = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Datos", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Datos",
+                columns: new[] { "Id", "Apellido", "Correo", "Nombre", "Telefono" },
+                values: new object[,]
+                {
+                    { 1, "De León", "dleonRonny@gmail.com", "Ronny", "809-865-2927" },
+                    { 2, "Almonte", "PipeAlm@gmail.com", "Pipe", "829-092-6767" },
+                    { 3, "Perez", "DiegoPPerez", "Diego", "809-000-0000" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Datos");
+        }
+    }
+}
